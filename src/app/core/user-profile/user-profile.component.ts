959cc3b15e66f6ed718c0f0e2b3cfeefae7ac013
valentin-zhelyazkov <56276041+valentin-zhelyazkov@users.noreply.github.com>
2021-08-02 15:32:41 +0300

    Displey username on profile page

--- src/app/core/user-profile/user-profile.component.ts
+++ src/app/core/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+
+  constructor(private afAuth: AngularFireAuth) { 
+    this.username = '';
+  }
 
   ngOnInit(): void {
+    this.afAuth.currentUser.then(res => {
+      this.username = res?.email ? res.email : '';
+    })
   }
 
 }
