466fd79bb8c1c172e464e085d236d5b4469dfee5
valentin-zhelyazkov <56276041+valentin-zhelyazkov@users.noreply.github.com>
2021-08-07 15:52:32 +0300

    fixing style

--- src/app/core/user-profile/user-profile.component.ts
+++ src/app/core/user-profile/user-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -9,14 +11,24 @@ import { AngularFireAuth } from '@angular/fire/auth';
 export class UserProfileComponent implements OnInit {
 
   username: string;
+  counter: number;
 
-  constructor(private afAuth: AngularFireAuth) { 
+  constructor(private afAuth: AngularFireAuth, private router: Router, private authService: AuthService) {
     this.username = '';
+    this.counter = 0;
   }
 
   ngOnInit(): void {
     this.afAuth.currentUser.then(res => {
       this.username = res?.email ? res.email : '';
+    })
+  }
+
+  deleteUser(): void {
+    this.afAuth.currentUser.then(res => {
+      res?.delete();
+      this.authService.logout();
+      this.router.navigate(['/']);
     })
   }
 
